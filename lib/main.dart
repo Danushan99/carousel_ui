af01e4d5bf1017ea132ca2f9fbe3eb1524792b2f
Danushan <it20051648@my.sliit.lk>
2021-12-25 13:43:22 +0530

    finish ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp((const MaterialApp(
@@ -14,9 +16,9 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   final List<List<String>> products = [
-    ['assets/images/w1.jpeg', 'watch name super', '19 \$'],
-    ['assets/images/w2.jpeg', 'watch name super', '19 \$'],
-    ['assets/images/w3.jpeg', 'watch name super', '19 \$'],
+    ['assets/images/w1.jpeg', 'Nomos Glashuette', '169 \$'],
+    ['assets/images/w2.jpeg', 'Hugo Boss White ', '189 \$'],
+    ['assets/images/w3.jpeg', 'Emporio Armani Classic', '199 \$'],
   ];
   int currentIndex = 0;
   void _next() {
@@ -42,79 +44,152 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              onHorizontalDragEnd: (DragEndDetails details) {
-                if (details.velocity.pixelsPerSecond.dx > 0) {
-                  _preve();
-                } else if (details.velocity.pixelsPerSecond.dx < 0) {
-                  _next();
-                }
-              },
-              child: Container(
-                width: double.infinity,
-                height: 550,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(products[currentIndex][0]),
-                    fit: BoxFit.cover,
-                  ),
+      body: Column(
+        children: <Widget>[
+          GestureDetector(
+            onHorizontalDragEnd: (DragEndDetails details) {
+              if (details.velocity.pixelsPerSecond.dx > 0) {
+                _preve();
+              } else if (details.velocity.pixelsPerSecond.dx < 0) {
+                _next();
+              }
+            },
+            child: Container(
+              width: double.infinity,
+              height: 550,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(products[currentIndex][0]),
+                  fit: BoxFit.cover,
                 ),
-                child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.bottomRight,
-                        colors: [
-                          Colors.grey[700]!.withOpacity(.9),
-                          Colors.grey.withOpacity(.0)
-                        ],
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Container(
-                          width: 90,
-                          margin: EdgeInsets.only(bottom: 60),
-                          child: Row(
-                            children: _buildIndicator(),
-                          ),
-                        )
-                      ],
-                    )),
               ),
-            ),
-            Expanded(
-              child: Transform.translate(
-                offset: Offset(0, -40),
-                child: Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(30),
+              child: Container(
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30)),
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomRight,
+                      colors: [
+                        Colors.grey[900]!.withOpacity(.9),
+                        Colors.grey.withOpacity(.0)
+                      ],
+                    ),
                   ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        products[currentIndex][1],
-                        style: TextStyle(
-                            color: Colors.grey[800],
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        width: 90,
+                        margin: const EdgeInsets.only(bottom: 60),
+                        child: Row(
+                          children: _buildIndicator(),
+                        ),
                       )
                     ],
-                  ),
+                  )),
+            ),
+          ),
+          Expanded(
+            child: Transform.translate(
+              offset: const Offset(0, -40),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(30),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      products[currentIndex][1],
+                      style: TextStyle(
+                          color: Colors.grey[800],
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          products[currentIndex][2],
+                          style: TextStyle(
+                              color: Colors.yellow[700],
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color: Colors.yellow[700],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color: Colors.yellow[700],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color: Colors.yellow[700],
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 20,
+                              color: Colors.yellow[700],
+                            ),
+                            Icon(
+                              Icons.star_half,
+                              size: 20,
+                              color: Colors.yellow[700],
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const SizedBox(
+                          height: 35,
+                        ),
+                        Text(
+                          ' (4.5/70 Reviews)',
+                          style:
+                              TextStyle(color: Colors.green[500], fontSize: 13),
+                        )
+                      ],
+                    ),
+                    Expanded(
+                        child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.yellow[700],
+                            borderRadius: BorderRadius.circular(8)),
+                        child: const Center(
+                          child: Text(
+                            "ADD TO CARD",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ))
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -123,11 +198,11 @@ class _HomepageState extends State<Homepage> {
     return Expanded(
         child: Container(
       height: 4,
-      margin: EdgeInsets.only(
+      margin: const EdgeInsets.only(
         right: 5,
       ),
       decoration:
-          BoxDecoration(color: isActiv ? Colors.grey[800] : Colors.white),
+          BoxDecoration(color: isActiv ? Colors.yellow[700] : Colors.white),
     ));
   }
 
